e8be543d8b3068c93e7649325a9ef10cc53ed556
Naveen Kumar G <gynaveenkumar7@gmail.com>
2026-06-06 21:20:08 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,15 @@
-content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+@type {import('tailwindcss').Config}
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+}
