604e4200b0ea1f152cd311ca8e2adac885906be8
Naveen Kumar G <gynaveenkumar7@gmail.com>
2026-06-24 11:45:28 +0530

    implementing analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import ShareMealLogo from './ShareMealLogo';
 import { useState, useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 import { MapPin, PhoneCall, Send, LogOut, Compass, Search, ArrowRight, Heart, Star, Filter, Plus, Flame, Sparkles, Camera, Building, User, UserCircle, Settings, Shield, Award, CheckCircle2, Menu, X} from 'lucide-react';
 
 //Environment variables
